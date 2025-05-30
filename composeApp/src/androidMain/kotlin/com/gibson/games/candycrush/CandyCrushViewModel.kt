8e12038210b3aefc31ee0dcf7b20f9ae1c3be814
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-30 15:39:18 +0100

    Update CandyCrushViewModel.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/candycrush/CandyCrushViewModel.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/candycrush/CandyCrushViewModel.kt
@@ -3,72 +3,55 @@ package com.gibson.games.candycrush
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlin.random.Random
 
 class CandyCrushViewModel : ViewModel() {
-    private val _board = MutableStateFlow(generateSafeBoard())
+
+    private val _board = MutableStateFlow(generateInitialBoard())
     val board: StateFlow<List<List<String>>> = _board
 
     private val _score = MutableStateFlow(0)
     val score: StateFlow<Int> = _score
 
-    private val candies = listOf("🍒", "🍋", "🍇", "🍉", "🍍", "🍓")
+    private fun generateInitialBoard(): List<List<String>> {
+        return List(8) { List(8) { randomCandy() } }.map { it.toMutableList() }
+    }
 
-    private fun randomCandy(): String = candies.random()
-
-    private fun generateSafeBoard(): List<List<String>> {
-        val board = MutableList(8) { MutableList(8) { "" } }
-
-        for (i in 0..7) {
-            for (j in 0..7) {
-                var candy: String
-                do {
-                    candy = randomCandy()
-                } while (
-                    (j >= 2 && candy == board[i][j - 1] && candy == board[i][j - 2]) ||
-                    (i >= 2 && candy == board[i - 1][j] && candy == board[i - 2][j])
-                )
-                board[i][j] = candy
-            }
-        }
-
-        return board
+    private fun randomCandy(): String {
+        val candies = listOf("🍒", "🍋", "🍇", "🍉", "🍍", "🍓")
+        return candies.random()
     }
 
     fun swapCandies(row1: Int, col1: Int, row2: Int, col2: Int) {
-        if (!isInBounds(row1, col1) || !isInBounds(row2, col2)) return
-
         val newBoard = _board.value.map { it.toMutableList() }.toMutableList()
         val temp = newBoard[row1][col1]
         newBoard[row1][col1] = newBoard[row2][col2]
         newBoard[row2][col2] = temp
-
         _board.value = newBoard
         processMatches()
     }
 
-    private fun isInBounds(i: Int, j: Int) = i in 0..7 && j in 0..7
-
     private fun processMatches() {
-        val board = _board.value.map { it.toMutableList() }
+        val board = _board.value.map { it.toMutableList() }.toMutableList()
         val matched = mutableSetOf<Pair<Int, Int>>()
 
-        // Horizontal
         for (i in board.indices) {
-            for (j in 0..5) {
-                val c = board[i][j]
-                if (c != "" && c == board[i][j + 1] && c == board[i][j + 2]) {
-                    matched += listOf(i to j, i to j + 1, i to j + 2)
+            for (j in 0..board[i].size - 3) {
+                val item = board[i][j]
+                if (item != "" && item == board[i][j + 1] && item == board[i][j + 2]) {
+                    matched.add(i to j)
+                    matched.add(i to j + 1)
+                    matched.add(i to j + 2)
                 }
             }
         }
 
-        // Vertical
         for (j in board[0].indices) {
-            for (i in 0..5) {
-                val c = board[i][j]
-                if (c != "" && c == board[i + 1][j] && c == board[i + 2][j]) {
-                    matched += listOf(i to j, i + 1 to j, i + 2 to j)
+            for (i in 0..board.size - 3) {
+                val item = board[i][j]
+                if (item != "" && item == board[i + 1][j] && item == board[i + 2][j]) {
+                    matched.add(i to j)
+                    matched.add(i + 1 to j)
+                    matched.add(i + 2 to j)
                 }
             }
         }
@@ -84,10 +67,15 @@ class CandyCrushViewModel : ViewModel() {
 
     private fun refillBoard(board: MutableList<MutableList<String>>) {
         for (j in board[0].indices) {
-            val column = board.map { it[j] }.filter { it != "" }
-            val missing = 8 - column.size
-            val newColumn = List(missing) { randomCandy() } + column
-            for (i in 0..7) board[i][j] = newColumn[i]
+            val column = mutableListOf<String>()
+            for (i in board.indices) {
+                if (board[i][j] != "") column.add(board[i][j])
+            }
+            val blanks = List(board.size - column.size) { randomCandy() }
+            val newColumn = blanks + column
+            for (i in board.indices) {
+                board[i][j] = newColumn[i]
+            }
         }
         _board.value = board
         processMatches()
