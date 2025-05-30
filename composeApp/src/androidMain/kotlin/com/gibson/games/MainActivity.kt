e6361063320069c8fe91fc6564325d588fdc77dc
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-30 14:16:19 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
@@ -1,27 +1,20 @@
+// MainActivity.kt
 package com.gibson.games
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.* 
-import androidx.compose.ui.Alignment 
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp 
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.gibson.games.candycrush.CandyCrushScreen
 import com.gibson.games.candycrush.CandyCrushViewModel
 
 class MainActivity : ComponentActivity() {
-      val candyCrushViewModel: CandyCrushViewModel by viewModels()
+    private val candyCrushViewModel: CandyCrushViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-        GamesApp(viewModel = candyCrushViewModel)
+            CandyCrushScreen(viewModel = candyCrushViewModel, onBack = {})
         }
     }
 }
