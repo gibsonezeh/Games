a8063ba9c21fd3b9c8e17b28a2b79464ae89d6d5
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-30 14:21:49 +0100

    Update CandyCrushViewModel.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/candycrush/CandyCrushViewModel.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/candycrush/CandyCrushViewModel.kt
@@ -1,27 +1,23 @@
+// CandyCrushViewModel.kt
 package com.gibson.games.candycrush
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlin.random.Random
 
 class CandyCrushViewModel : ViewModel() {
-
-    private val _board = MutableStateFlow(generateInitialBoard().map { it.toMutableList() }.toMutableList())
+    private val _board = MutableStateFlow(generateInitialBoard())
     val board: StateFlow<List<List<String>>> = _board
 
     private val _score = MutableStateFlow(0)
     val score: StateFlow<Int> = _score
 
-    private val numRows = 8
-    private val numCols = 8
-
-    init {
-        processMatchesRecursively()
-    }
-
-    private fun generateInitialBoard(): List<List<String>> {
-        return List(numRows) { List(numCols) { randomCandy() } }
+    private fun generateInitialBoard(): MutableList<MutableList<String>> {
+        val board = MutableList(8) { MutableList(8) { randomCandy() } }
+        while (hasMatches(board)) {
+            processMatches(board)
+        }
+        return board
     }
 
     private fun randomCandy(): String {
@@ -31,65 +27,62 @@ class CandyCrushViewModel : ViewModel() {
 
     fun swapCandies(row1: Int, col1: Int, row2: Int, col2: Int) {
         val newBoard = _board.value.map { it.toMutableList() }.toMutableList()
-
         val temp = newBoard[row1][col1]
         newBoard[row1][col1] = newBoard[row2][col2]
         newBoard[row2][col2] = temp
-
         _board.value = newBoard
-        processMatchesRecursively()
+        processMatches(newBoard)
     }
 
-    private fun processMatchesRecursively() {
-        val board = _board.value.map { it.toMutableList() }.toMutableList()
+    private fun hasMatches(board: List<List<String>>): Boolean {
+        return detectMatches(board).isNotEmpty()
+    }
+
+    private fun detectMatches(board: List<List<String>>): Set<Pair<Int, Int>> {
         val matched = mutableSetOf<Pair<Int, Int>>()
 
-        // Horizontal matches
-        for (i in 0 until numRows) {
-            for (j in 0 until numCols - 2) {
-                val candy = board[i][j]
-                if (candy != "" && candy == board[i][j + 1] && candy == board[i][j + 2]) {
-                    matched.add(i to j)
-                    matched.add(i to j + 1)
-                    matched.add(i to j + 2)
+        for (i in board.indices) {
+            for (j in 0 until board[i].size - 2) {
+                val item = board[i][j]
+                if (item != "" && item == board[i][j + 1] && item == board[i][j + 2]) {
+                    matched.addAll(listOf(i to j, i to j + 1, i to j + 2))
                 }
             }
         }
 
-        // Vertical matches
-        for (j in 0 until numCols) {
-            for (i in 0 until numRows - 2) {
-                val candy = board[i][j]
-                if (candy != "" && candy == board[i + 1][j] && candy == board[i + 2][j]) {
-                    matched.add(i to j)
-                    matched.add(i + 1 to j)
-                    matched.add(i + 2 to j)
+        for (j in board[0].indices) {
+            for (i in 0 until board.size - 2) {
+                val item = board[i][j]
+                if (item != "" && item == board[i + 1][j] && item == board[i + 2][j]) {
+                    matched.addAll(listOf(i to j, i + 1 to j, i + 2 to j))
                 }
             }
         }
 
-        if (matched.isNotEmpty()) {
-            matched.forEach { (i, j) -> board[i][j] = "" }
-            _score.value += matched.size * 10
+        return matched
+    }
+
+    private fun processMatches(board: MutableList<MutableList<String>>) {
+        val matches = detectMatches(board)
+        if (matches.isNotEmpty()) {
+            matches.forEach { (i, j) -> board[i][j] = "" }
+            _score.value += matches.size * 10
             refillBoard(board)
-            _board.value = board
-            processMatchesRecursively() // Continue checking after refill
-        } else {
-            _board.value = board
         }
+        _board.value = board
     }
 
     private fun refillBoard(board: MutableList<MutableList<String>>) {
-        for (j in 0 until numCols) {
-            val column = mutableListOf<String>()
-            for (i in 0 until numRows) {
-                if (board[i][j] != "") column.add(board[i][j])
-            }
-            val blanks = List(numRows - column.size) { randomCandy() }
+        for (j in board[0].indices) {
+            val column = board.map { it[j] }.filter { it != "" }.toMutableList()
+            val blanks = MutableList(board.size - column.size) { randomCandy() }
             val newColumn = blanks + column
-            for (i in 0 until numRows) {
+            for (i in board.indices) {
                 board[i][j] = newColumn[i]
             }
+        }
+        if (hasMatches(board)) {
+            processMatches(board)
         }
     }
 }
