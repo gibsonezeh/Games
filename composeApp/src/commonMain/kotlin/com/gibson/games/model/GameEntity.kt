8e438f1d3366bf308875f235a18a27aa58a6e34a
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-07 21:24:57 +0100

    Update GameEntity.kt

--- composeApp/src/commonMain/kotlin/com/gibson/games/model/GameEntity.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/games/model/GameEntity.kt
@@ -1,1 +1,16 @@
+package com.gibson.games.model
 
+import com.gibson.games.core.Vector2
+
+open class GameEntity(
+    var position: Vector2,
+    var width: Float,
+    var height: Float
+) {
+    fun isCollidingWith(other: GameEntity): Boolean {
+        return position.x < other.position.x + other.width &&
+               position.x + width > other.position.x &&
+               position.y < other.position.y + other.height &&
+               position.y + height > other.position.y
+    }
+}
