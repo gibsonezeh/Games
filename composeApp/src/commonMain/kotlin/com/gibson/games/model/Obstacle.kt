fa12f3abee2e4939328027ae5a9100031222fd0f
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-07 20:13:08 +0100

    Update Obstacle.kt

--- composeApp/src/commonMain/kotlin/com/gibson/games/model/Obstacle.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/games/model/Obstacle.kt
@@ -1,1 +1,10 @@
+package com.gibson.games.model
 
+import com.gibson.games.core.Vector2
+
+data class Obstacle(
+    var position: Vector2,
+    val width: Float = 100f,
+    val height: Float = 100f,
+    val lane: Int = 1 // Lane 0, 1, or 2
+)
