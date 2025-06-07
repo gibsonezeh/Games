8b08e7fa91d08a24fa194053f07af82ee2eed406
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-07 20:58:10 +0100

    Update GameLoop.kt

--- composeApp/src/commonMain/kotlin/com/gibson/games/core/GameLoop.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/games/core/GameLoop.kt
@@ -1,1 +1,20 @@
+package com.gibson.games.core
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+
+/**
+ * Executes the given `onFrame` lambda every frame (~60 FPS).
+ */
+@Composable
+fun GameLoop(
+    isRunning: Boolean,
+    onFrame: () -> Unit
+) {
+    LaunchedEffect(isRunning) {
+        while (isRunning) {
+            onFrame()
+            kotlinx.coroutines.delay(16L) // ~60 FPS (1000ms / 60fps = ~16ms)
+        }
+    }
+}
