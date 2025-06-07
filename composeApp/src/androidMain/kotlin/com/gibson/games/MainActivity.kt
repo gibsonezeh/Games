78706a1b805422890558a1a24f8b945430612d6b
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-07 15:13:29 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
@@ -1,15 +1,45 @@
 package com.gibson.games
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.gibson.games.platform.input.AndroidSwipeDetector
 import com.gibson.games.ui.MenuScreen
+import com.gibson.games.core.SwipeDirection
+import com.gibson.games.core.SwipeInputHandler // Optional interface or object to relay swipes
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val swipeDetector = AndroidSwipeDetector { direction ->
+            // Dispatch to global swipe handler (to be implemented)
+            SwipeInputHandler.handleSwipe(direction)
+        }
+
         setContent {
-            MenuScreen()
+            Box(modifier = Modifier.fillMaxSize()) {
+
+                // Invisible AndroidView to capture swipe gestures
+                AndroidView(
+                    factory = { context ->
+                        View(context).apply {
+                            setOnTouchListener(swipeDetector)
+                        }
+                    },
+                    modifier = Modifier.fillMaxSize()
+                )
+
+                // Game UI on top
+                MenuScreen()
+            }
         }
     }
 }
