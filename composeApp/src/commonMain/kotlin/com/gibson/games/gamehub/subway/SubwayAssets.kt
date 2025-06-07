0c23450e12f4155c1e23f654eef3f28e2b91652f
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-07 21:17:58 +0100

    Update SubwayAssets.kt

--- composeApp/src/commonMain/kotlin/com/gibson/games/gamehub/subway/SubwayAssets.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/games/gamehub/subway/SubwayAssets.kt
@@ -1,1 +1,12 @@
+package com.gibson.games.gamehub.subway
 
+object SubwayAssets {
+    // Placeholder for images, sounds, or animations
+    // In Compose Multiplatform, you might use Resource APIs for platform-specific assets
+
+    val playerColor = androidx.compose.ui.graphics.Color.Green
+    val obstacleColor = androidx.compose.ui.graphics.Color.Red
+    val backgroundColor = androidx.compose.ui.graphics.Color.Black
+
+    // TODO: Load actual assets (images, audio, etc.) when resource APIs are in place
+}
