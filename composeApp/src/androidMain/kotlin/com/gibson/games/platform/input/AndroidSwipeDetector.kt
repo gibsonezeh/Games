310f99b4f7dd70b0779b5f157e290be03ece7a0e
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-07 15:07:39 +0100

    Update AndroidSwipeDetector.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/platform/input/AndroidSwipeDetector.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/platform/input/AndroidSwipeDetector.kt
@@ -1,1 +1,51 @@
 
+package com.gibson.games.platform.input
+
+import android.view.MotionEvent
+import android.view.View
+import kotlin.math.abs
+import com.gibson.games.core.SwipeDirection
+
+class AndroidSwipeDetector(
+    private val onSwipe: (SwipeDirection) -> Unit
+) : View.OnTouchListener {
+
+    private var downX = 0f
+    private var downY = 0f
+
+    override fun onTouch(view: View?, event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                downX = event.x
+                downY = event.y
+                return true
+            }
+
+            MotionEvent.ACTION_UP -> {
+                val upX = event.x
+                val upY = event.y
+
+                val deltaX = upX - downX
+                val deltaY = upY - downY
+
+                if (abs(deltaX) > abs(deltaY)) {
+                    // Horizontal swipe
+                    if (deltaX > 100) {
+                        onSwipe(SwipeDirection.RIGHT)
+                    } else if (deltaX < -100) {
+                        onSwipe(SwipeDirection.LEFT)
+                    }
+                } else {
+                    // Vertical swipe
+                    if (deltaY > 100) {
+                        onSwipe(SwipeDirection.DOWN)
+                    } else if (deltaY < -100) {
+                        onSwipe(SwipeDirection.UP)
+                    }
+                }
+                return true
+            }
+        }
+        return false
+    }
+}
