269c986c8c4c741de824057ab5d868994151f858
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-08 22:18:06 +0100

    Delete composeApp/src/androidMain/kotlin/com/gibson/games/SwipeDetector.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/SwipeDetector.kt
+++
@@ -1,8 +0,0 @@
-package com.gibson.games
-
-import androidx.compose.ui.input.pointer.PointerInputScope
-import com.gibson.games.core.SwipeDirection
-
-interface SwipeDetector {
-    fun detectSwipe(pointerInputScope: PointerInputScope, onSwipe: (SwipeDirection) -> Unit)
-}
