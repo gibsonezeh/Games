fd7535c78eb93cec4dd67a00216da98749bf4c85
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-08 22:01:28 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
@@ -1,24 +1,17 @@
+// androidMain/com/gibson/games/MainActivity.kt
+
 package com.gibson.games
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.gibson.games.gamehub.subway.SubwayScreen
+import com.gibson.games.ui.MultiGameApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SubwayScreen()
+            MultiGameApp()
         }
     }
 }
-
-@Preview
-@Composable
-fun AppPreview() {
-    SubwayScreen()
-}
-
