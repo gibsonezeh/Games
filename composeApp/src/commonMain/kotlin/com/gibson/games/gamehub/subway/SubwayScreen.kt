aaf10da19d1d2e3c51b36f83bc2b00326104ec1f
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-08 18:13:51 +0100

    Update SubwayScreen.kt

--- composeApp/src/commonMain/kotlin/com/gibson/games/gamehub/subway/SubwayScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/games/gamehub/subway/SubwayScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import com.gibson.games.AndroidSwipeDetector
 
 // Assuming this is the common interface
 
