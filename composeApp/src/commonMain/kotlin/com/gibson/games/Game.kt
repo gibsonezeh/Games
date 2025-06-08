074e55783038800286e95fdc8e4c0d8b4c28d4fa
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-08 22:04:20 +0100

    Create Game.kt

---
+++ composeApp/src/commonMain/kotlin/com/gibson/games/Game.kt
@@ -0,0 +1,8 @@
+// commonMain/com/gibson/games/Game.kt
+
+package com.gibson.games
+
+enum class Game(val displayName: String) {
+    NinjaAssassin("Ninja Assassin")
+    // Add future games here
+}
