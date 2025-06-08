6a119b79fed3d1403ee40c4ae5444ced93022a29
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-06-08 14:29:09 +0000

    Add files via upload

--- composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
@@ -1,45 +1,24 @@
 package com.gibson.games
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
-import com.gibson.games.platform.input.AndroidSwipeDetector
-import com.gibson.games.ui.MenuScreen
-import com.gibson.games.core.SwipeDirection
-import com.gibson.games.core.SwipeInputHandler // Optional interface or object to relay swipes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.gibson.games.gamehub.subway.SubwayScreen
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val swipeDetector = AndroidSwipeDetector { direction ->
-            // Dispatch to global swipe handler (to be implemented)
-            SwipeInputHandler.handleSwipe(direction)
-        }
-
         setContent {
-            Box(modifier = Modifier.fillMaxSize()) {
-
-                // Invisible AndroidView to capture swipe gestures
-                AndroidView(
-                    factory = { context ->
-                        View(context).apply {
-                            setOnTouchListener(swipeDetector)
-                        }
-                    },
-                    modifier = Modifier.fillMaxSize()
-                )
-
-                // Game UI on top
-                MenuScreen()
-            }
+            SubwayScreen()
         }
     }
 }
+
+@Preview
+@Composable
+fun AppPreview() {
+    SubwayScreen()
+}
+
