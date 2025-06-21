c2008a1f058df72c93ca79f48f9a44af19ee17e9
user <gibsonezeh6@gmail.com>
2025-06-21 22:59:56 +0000

    done

--- composeApp/src/wasmJsMain/kotlin/com/gibson/games/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/gibson/games/main.kt
@@ -1,1 +1,13 @@
+package com.gibson.games
 
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport(document.body!!) {
+        App()
+    }
+
+}
