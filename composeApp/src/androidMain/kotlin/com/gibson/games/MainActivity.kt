f7cbcc9612d8267c488c1a46bd29a223a3ec5851
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2026-04-02 00:34:07 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gibson.games
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.gibson.games.ads.AdMobBanner
 import com.gibson.games.ui.MultiGameApp
 import com.google.android.gms.ads.MobileAds
 
@@ -10,11 +11,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 🔥 Initialize AdMob
         MobileAds.initialize(this)
 
         setContent {
-            MultiGameApp()
+            MultiGameApp(
+                bottomContent = {
+                    AdMobBanner()
+                }
+            )
         }
     }
 }
