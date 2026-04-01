36b2d5db3cc893f0619201afeacce11328889009
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2026-04-02 00:24:26 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/MainActivity.kt
@@ -1,15 +1,18 @@
-// androidMain/com/gibson/games/MainActivity.kt
-
 package com.gibson.games
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.gibson.games.ui.MultiGameApp
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 🔥 Initialize AdMob
+        MobileAds.initialize(this)
+
         setContent {
             MultiGameApp()
         }
