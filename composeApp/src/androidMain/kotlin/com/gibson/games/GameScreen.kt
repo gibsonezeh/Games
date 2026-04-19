840ac5f8176104e84f41498b35e88d5bed0ee0eb
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2026-04-19 16:51:52 +0100

    Update GameScreen.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/GameScreen.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/GameScreen.kt
@@ -1,11 +1,8 @@
 package com.gibson.games
 
-import androidx.compose.runtime.*
-import com.gibson.games.ludo.GameRules
-import com.gibson.games.ludo.LudoGameScreen
-import com.gibson.games.ludo.LudoMainMenuScreen
+import androidx.compose.runtime.Composable
+import com.gibson.games.ludo.LudoNavigationScreen
 import com.gibson.games.zuma.ui.ZumaNavigationScreen
-import com.gibson.games.ludo.LudoSettingsScreen
 
 @Composable
 fun GameScreen(
@@ -14,58 +11,6 @@ fun GameScreen(
 ) {
     when (game) {
         Game.LUDO -> LudoNavigationScreen(onExit = onExit)
-        
-        Game.ZUMA -> ZumaNavigationScreen(onExit = onExit) // ✅ NEW
+        Game.ZUMA -> ZumaNavigationScreen(onExit = onExit)
     }
-}
-
-private enum class LudoScreen {
-    MAIN_MENU,
-    GAME,
-    SETTINGS
-}
-
-@Composable
-fun LudoNavigationScreen(
-    onExit: () -> Unit
-) {
-    var currentScreen by remember { mutableStateOf(LudoScreen.MAIN_MENU) }
-    var gameRules by remember { mutableStateOf(GameRules()) }
-
-    when (currentScreen) {
-        LudoScreen.MAIN_MENU -> {
-            LudoMainMenuScreen(
-                onPlayClicked = {
-                    currentScreen = LudoScreen.GAME
-                },
-                onExitClicked = onExit,
-                onSettingsClicked = {
-                    currentScreen = LudoScreen.SETTINGS
-                }
-            )
-        }
-
-        LudoScreen.GAME -> {
-            LudoGameScreen(
-                onExit = {
-                    currentScreen = LudoScreen.MAIN_MENU
-                },
-                gameRules = gameRules,
-
-            )
-        }
-
-        LudoScreen.SETTINGS -> {
-            LudoSettingsScreen(
-                onBackClicked = {
-                    currentScreen = LudoScreen.MAIN_MENU
-                },
-                gameRules = gameRules,
-                onRulesChanged = { updatedRules ->
-                    gameRules = updatedRules
-                }
-            )
-        }
-    }
-
 }
