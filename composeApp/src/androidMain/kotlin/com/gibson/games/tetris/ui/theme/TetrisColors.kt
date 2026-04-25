385934f92054ba0b3c597d2227156a6fa58a61d3
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2026-04-25 18:36:27 +0100

    Create TetrisColors.kt

---
+++ composeApp/src/androidMain/kotlin/com/gibson/games/tetris/ui/theme/TetrisColors.kt
@@ -0,0 +1,16 @@
+package com.gibson.games.tetris.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TetrisBody = Color(0xFFFFD600)
+
+val TetrisScreenBackground = Color(0xFF9AAA82)
+
+val TetrisBrick = Color(0xFF1D2418)
+val TetrisBrickGhost = Color(0xFF87936F)
+
+val TetrisButtonTop = Color(0xFFB76CFF)
+val TetrisButtonBottom = Color(0xFF6200EE)
+
+val TetrisBorderDark = Color(0x88000000)
+val TetrisBorderLight = Color(0x88FFFFFF)
