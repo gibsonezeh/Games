0d97de6d8972c5c3ca70621429a1cf17c16e76a8
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2026-04-26 11:21:45 +0100

    Update TetrisState.kt

--- composeApp/src/androidMain/kotlin/com/gibson/games/tetris/engine/TetrisState.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/games/tetris/engine/TetrisState.kt
@@ -50,13 +50,19 @@ enum class TetrisGameStatus {
 
 sealed interface TetrisAction {
     data class Move(val direction: Direction) : TetrisAction
+
+    data object Rotate : TetrisAction
+    data object Drop : TetrisAction
+
     data object Reset : TetrisAction
     data object Pause : TetrisAction
     data object Resume : TetrisAction
-    data object Rotate : TetrisAction
-    data object Drop : TetrisAction
     data object GameTick : TetrisAction
     data object Mute : TetrisAction
+
+    data object StartGame : TetrisAction
+    data class SetLevel(val delta: Int) : TetrisAction
+    data class SetStartLines(val delta: Int) : TetrisAction
 }
 
 const val MATRIX_WIDTH = 12
